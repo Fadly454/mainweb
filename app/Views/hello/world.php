b79a60a130d1918f65ad9b6a0b592ce0f7a3e47b
unknown <muhammad1800016149@webmail.uad,ac,id>
2022-12-28 16:12:51 +0700

    First Push

---
+++ app/Views/hello/world.php
@@ -0,0 +1,7 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+
+<h1> Welcome To Diby Shop </h1>
+<?= $data ?>
+
+<?= $this->endSection() ?>
---
+++ app/Views/logout.php
@@ -0,0 +1,6 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+
+<h1> Form Logout </h1>
+
+<?= $this->endSection() ?>
